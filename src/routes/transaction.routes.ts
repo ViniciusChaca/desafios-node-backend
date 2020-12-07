2bb018d71481760602920cd78e7f27b0e1176f72
Vinicius Chaca <vinicius.e.ferreira@gmail.com>
2020-12-07 19:06:25 -0300

    Correção no código e testes passados

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -11,8 +11,9 @@ const transactionsRepository = new TransactionsRepository();
 transactionRouter.get('/', (request, response) => {
   try {
     const transactions = transactionsRepository.all();
+    const balance = transactionsRepository.getBalance();
 
-    return response.json(transactions);
+    return response.json({transactions, balance});
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
@@ -25,9 +26,8 @@ transactionRouter.post('/', (request, response) => {
     const createTransaction = new CreateTransactionService(transactionsRepository);
 
     const transaction = createTransaction.execute({ title, value, type });
-    const balance = transactionsRepository.getBalance();
 
-    return response.json({ transaction, balance });
+    return response.json(transaction);
 
 
   } catch (err) {
